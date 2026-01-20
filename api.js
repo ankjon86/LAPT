4b911b5eb23526d4e00e2767c286faa198c71301
ankjon86 <ankjon86@gmail.com>
2026-01-20 08:25:34 +0000

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 class ApiService {
   constructor() {
     // Your Google Apps Script Web App URL
-    this.BASE_URL = 'https://script.google.com/macros/library/d/1ak-zLKC_xoyvAn3vOlpu4dBopqAdwoAaduAniMWPqcTqOevbRLksS6g7/15';
+    this.BASE_URL = 'https://script.google.com/macros/s/AKfycbylE1YhW-h5CddXCSCDdfj2co-JYOg8PdBm5ZAj49DqLUOId1bYeoBZGRruQcFuNzaMZg/exec';
     this.cache = new Map();
     this.requestCount = 0;
     this.activeRequests = new Map();
